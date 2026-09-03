d60467e7065876d21f6827218b94c2edfbd4f379
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-09-03 04:35:37 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -87,11 +87,10 @@
     map = L.map("map", { zoomControl: false, attributionControl: true })
       .setView([currentPos.lat, currentPos.lon], 19);
 
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      maxNativeZoom: 19,
+    L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}", {
+      attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+      maxNativeZoom: 16,
       maxZoom: 22,
-      subdomains: "abcd",
     }).addTo(map);
 
     playerMarker = L.marker([currentPos.lat, currentPos.lon], {
