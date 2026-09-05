2775389a6999443f27f1598459d500850170c63b
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-09-04 17:56:34 -0700

    Update grid.js

--- js/grid.js
+++ js/grid.js
@@ -156,11 +156,6 @@ const Grid = (() => {
     const allPlots = getAllPlots();
     const zoom = map.getZoom();
 
-    // Update 3D GLTF Grass & Rarity Props in Three.js WebGL layer
-    if (typeof Character3D !== "undefined" && Character3D.updatePlots) {
-      Character3D.updatePlots(allPlots);
-    }
-
     // 1. RENDER CLAIMED PLOTS (Instantly)
     const claimedFeatures = [];
     for (const tid in allPlots) {
