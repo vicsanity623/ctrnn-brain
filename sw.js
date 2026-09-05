14f274b066df11968bb0bb6c86f65ddaa273f5b7
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-09-05 14:14:26 -0700

    Update cache version to v1.1.7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-earth-v1.1.6';
+const CACHE_NAME = 'elden-earth-v1.1.7';
 
 const ASSETS_TO_CACHE = [
     './',
