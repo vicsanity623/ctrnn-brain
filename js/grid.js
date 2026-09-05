8c7560b18e6ab0d432b3058405874df5fd314c37
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-09-04 18:37:29 -0700

    Update grid.js

--- js/grid.js
+++ js/grid.js
@@ -156,11 +156,6 @@ const Grid = (() => {
     const allPlots = getAllPlots();
     const zoom = map.getZoom();
 
-    // Render 3D Procedural Grass on claimed plots
-    if (typeof Character3D !== "undefined" && Character3D.updatePlots) {
-      Character3D.updatePlots(allPlots);
-    }
-
     // 1. RENDER CLAIMED PLOTS (Instantly)
     const claimedFeatures = [];
     for (const tid in allPlots) {
